5e12a6491b5dd6baa9fdd3da84a0f3808721f095
Damian Akpan <damiakpan@gmail.com>
2025-01-12 21:10:00 +0100

    Added transparency to app bar
    Signed-off-by: Damian Akpan <damiakpan@gmail.com>

--- src/app/(app)/home/_components/AppBar/index.tsx
+++ src/app/(app)/home/_components/AppBar/index.tsx
@@ -3,23 +3,40 @@ import React from "react";
 import { View, Text, StatusBar, StyleSheet } from "react-native";
 
 import { Image } from "expo-image";
+import { LinearGradient } from "expo-linear-gradient";
 
 import BellIcon from "@/assets/icons/bell.svg";
+
 
 const AppBar = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={textStyles.greetingText}>Good Morning,</Text>
-        <Text style={textStyles.mainText}>John Doe</Text>
+      <LinearGradient
+        style={{
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+        }}
+        colors={["rgba(255, 255, 255, 0.9)", "rgba(255, 255, 255, 0.28)"]}
+        locations={[0.6, 1]}
+      />
+
+      <View style={styles.textContainer}>
+        <View>
+          <Text style={textStyles.greetingText}>Good Morning,</Text>
+          <Text style={textStyles.mainText}>John Doe</Text>
+        </View>
+
+        <View style={styles.notificationContainer}>
+          <Image source={BellIcon} style={{
+            width: 24,
+            height: 24,
+          }} />
+        </View>
       </View>
 
-      <View style={styles.notificationContainer}>
-        <Image source={BellIcon} style={{
-          width: 24,
-          height: 24,
-        }} />
-      </View>
     </View>
   );
 };
@@ -29,16 +46,21 @@ const styles = StyleSheet.create({
   container: {
     top: 0,
     left: 0,
+    height: 120,
     zIndex: 100,
     width: "100%",
+    flexDirection: "row",
     position: "absolute",
+    alignItems: "flex-end",
+    // paddingTop: (StatusBar.currentHeight || 30) + 10,
+  },
+  textContainer: {
+    width: "100%",
     paddingBottom: 8,
     flexDirection: "row",
-    alignItems: "center",
     paddingHorizontal: 20,
-    backgroundColor: "white",
+    alignItems: "center",
     justifyContent: "space-between",
-    paddingTop: (StatusBar.currentHeight || 30) + 10,
   },
   notificationContainer: {
     padding: 10,
