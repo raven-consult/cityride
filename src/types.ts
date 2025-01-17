9f0c5c547ba26da65f05098985f879ecdb25a102
Damian Akpan <damiakpan@gmail.com>
2025-01-17 13:41:28 +0100

    Added types for user data into mobile
    Signed-off-by: Damian Akpan <damiakpan@gmail.com>

--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,24 @@ export interface Info {
   action?: () => void;
 }
 
+/// [START] User Type
+
+export type Role = "passenger" | "driver";
+
+export interface DriverInfo {
+  carNumber: string;
+  maxPassengers: number;
+}
+
+export interface UserData {
+  role: Role;
+  email: string;
+  displayName: string;
+  driverInfo?: DriverInfo;
+}
+
+/// [END] User Type
+
 export interface Ride {
   id: string;
   price: number;
